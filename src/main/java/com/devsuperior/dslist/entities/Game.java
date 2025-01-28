a5ad75fd26caa55fb9570d2dceef0ce5fddb7ad2
Alicia Munhoz <aliciamfc96@gmail.com>
2025-01-27 22:32:44 -0300

    feat: game repository, service e controller

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -24,12 +24,14 @@ public class Game {
     private String platforms;
     private Double score;
     private String imgUrl;
-    
+
     @Column(columnDefinition = "TEXT")
     private String shortDescription;
 
     @Column(columnDefinition = "TEXT")
     private String longDescription;
+
+    public Game() {}
 
     public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
