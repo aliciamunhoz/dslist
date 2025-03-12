f9ec575fe29810e7c4836f69136991943da43336
Alicia Munhoz <aliciamfc96@gmail.com>
2025-03-12 17:29:00 -0300

    feat: move method

---
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dslist.dto;
+
+public class ReplacementDTO {
+    private Integer sourceIndex;
+    private Integer destionationIndex;
+
+    public Integer getSourceIndex() {
+        return sourceIndex;
+    }
+
+    public void setSourceIndex(Integer sourceIndex) {
+        this.sourceIndex = sourceIndex;
+    }
+
+    public Integer getDestionationIndex() {
+        return destionationIndex;
+    }
+
+    public void setDestionationIndex(Integer destionationIndex) {
+        this.destionationIndex = destionationIndex;
+    }
+}
